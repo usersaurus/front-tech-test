ef66fa53c99ea9e4ab8593c14ecad79eeca63685
usersaurus <antluisroman@gmail.com>
2024-07-02 00:54:17 +0200

    🧹 Removed mocks that were moved to vitest setup in a previous commit

--- src/components/__tests__/back-button.test.tsx
+++ src/components/__tests__/back-button.test.tsx
@@ -1,29 +1,9 @@
-import { expect, describe, it, beforeAll, vi } from 'vitest';
+import { expect, describe, it, beforeAll } from 'vitest';
 import BackButton from '../back-button';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { useNavigate } from 'react-router-dom';
 
-
-vi.mock('@radix-ui/themes', async (importOriginal) => {
-  const mod = await importOriginal() as typeof importOriginal;
-
-  return {
-    ...mod,
-    Tooltip: ({ children }: { children: JSX.Element }) => children,
-  };
-});
-
-vi.mock('react-router-dom', async (importOriginal) => {
-  const mod = await importOriginal() as typeof importOriginal;
-
-  return {
-    ...mod,
-    useNavigate: vi.fn(() => vi.fn()),
-  };
-});
-
 describe('BackButton', () => {
-
   beforeAll(() => {
     render(<BackButton />);
   });

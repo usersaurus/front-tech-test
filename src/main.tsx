ef47c077245564321b6b0936467b75f74860b0d3
usersaurus <antluisroman@gmail.com>
2024-07-01 17:55:12 +0200

    🎨 Changed accent color

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Theme appearance='dark'>
+    <Theme appearance='dark' accentColor='ruby'>
       <App />
     </Theme>
   </React.StrictMode>,

47afc622e0c8e243602c7f88a05e6bae91d46a65
usersaurus <antluisroman@gmail.com>
2024-07-01 21:47:03 +0200

    🕵️ More tests on store

--- src/store/__tests__/store.test.ts
+++ src/store/__tests__/store.test.ts
@@ -31,4 +31,35 @@ describe('store', () => {
 
     expect(useSWStore.getState().planets).toEqual([earth]);
   });
+
+  it('should update planet', () => {
+    const firstPlanet = planets[0];
+    const updatedFirstPlanet = { ...firstPlanet, name: 'Updated Name' };
+
+    useSWStore.getState().setPlanets(planets);
+    useSWStore.getState().updatePlanet(updatedFirstPlanet);
+
+    expect(useSWStore.getState().planets[0]).toEqual(updatedFirstPlanet);
+  });
+
+  it('should get planet by id', () => {
+    useSWStore.getState().setPlanets(planets);
+
+    expect(useSWStore.getState().getPlanetById('1')).toEqual(planets[0]);
+  });
+
+  it('should get paginated planets', () => {
+    useSWStore.getState().setPlanets(planets);
+
+    expect(useSWStore.getState().getPaginatedPlanets(1, 2)).toEqual({
+      planets: [planets[0], planets[1]],
+      total: 2
+    });
+  });
+
+  it('should set last requested page', () => {
+    useSWStore.getState().setLastRequestedPage(2);
+
+    expect(useSWStore.getState().lastRequestedPage).toBe(2);
+  });
 });
